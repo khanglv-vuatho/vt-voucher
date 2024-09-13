b95c369b7f91a5db1f602f6292acb6389b7bab35
khanglv-vuatho <khanglv@vuatho.com>
2024-09-13 09:00:21 +0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,22 @@
 import { Route, Routes } from 'react-router-dom'
-import { lazy, Suspense } from 'react'
-import { CircularProgress } from '@nextui-org/react'
 
-const InvalidPage = lazy(() => import('./pages/invalid'))
-const HomePage = lazy(() => import('@/pages/index'))
+import Home from '@/pages/index'
+import InvalidPage from '@/pages/invalid'
+import Test from './pages/test'
 
 const routes = [
-  { path: '/chat', element: <HomePage /> },
-  { path: '/invalid', element: <InvalidPage /> }
+  { path: '/', element: <Home /> },
+  { path: '/invalid', element: <InvalidPage /> },
+  { path: '/test', element: <Test /> }
 ]
 
 function App() {
   return (
-    <Suspense
-      fallback={
-        <div className='flex h-dvh w-full items-center justify-center'>
-          <CircularProgress
-            classNames={{
-              svg: 'h-8 w-8 text-primary-blue'
-            }}
-          />
-        </div>
-      }
-    >
-      <Routes>
-        {routes.map(({ path, element }, index) => (
-          <Route key={index} path={path} element={element} />
-        ))}
-      </Routes>
-    </Suspense>
+    <Routes>
+      {routes.map(({ path, element }, index) => (
+        <Route key={index} path={path} element={element} />
+      ))}
+    </Routes>
   )
 }
 
