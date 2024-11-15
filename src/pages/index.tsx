5f54aa9475d56bb7face79223df9c295cfc79165
khanglv-vuatho <khanglv@vuatho.com>
2024-11-15 17:38:56 +0700

    update message of toast

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -246,7 +246,7 @@ const Home = () => {
   const handleCallApiSearchVoucher = async () => {
     try {
       const data: Coupon = await getVouchers({ orderId, searchValue })
-      if ((data as any).length === 0 || data?.can_apply === false) return ToastComponent({ message: 'Voucher không tồn tại', type: 'error' })
+      if ((data as any).length === 0 || data?.can_apply === false) return ToastComponent({ message: 'Voucher đã hết hạn hoặc không tồn tại', type: 'error' })
       setVoucherData((prev) => {
         const existingVoucher = prev?.find((voucher) => voucher?.id === data?.id)
         if (!existingVoucher) {
